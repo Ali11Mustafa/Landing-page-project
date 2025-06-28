7ca5d51ac8693d051c92600c34338b4bba13680e
Ali11Mustafa <ali.00622274@gmail.com>
2025-06-28 21:24:11 +0300

    changes

--- src/Components/Pages/BusinessPrices/PriceList/priceList.tsx
+++ src/Components/Pages/BusinessPrices/PriceList/priceList.tsx
@@ -5,7 +5,32 @@ import { FiChevronDown, FiChevronUp } from 'react-icons/fi';
 import { PricingPlan } from '../../../types/Pricing';
 import PriceBackground from '../../../../../public/price/PriceBackground.png';
 import { motion, AnimatePresence } from 'framer-motion';
-import { pricingData } from '../../../data/pricingData';
+
+
+const pricingData: PricingPlan[] = [
+ {
+  title: "Intro",
+  description: "The Intro plan offers essential features, allowing you to upload HD videos with ease while catering to basic business requirements.",
+  price: "Free"
+},
+{
+  title: "Base",
+  description: "The Base plan is designed for small teams, providing robust tools to support your growing business needs.",
+  price: "$49"
+},
+{
+  title: "Pro",
+  description: "The Pro plan enables advanced capabilities, including unlimited HD video uploads and comprehensive features tailored to meet the demands of professional users.",
+  price: "$123",
+  badge: "Save $40",
+  button: "Try 1 month"
+},
+{
+  title: "Enterprise",
+  description: "The Enterprise plan delivers a complete suite of solutions designed for large-scale operations and customized business requirements.",
+  price: "Custom"
+}
+];
 
 
 interface PricingCardProps {
