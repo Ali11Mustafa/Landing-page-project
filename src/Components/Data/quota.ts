072369845143ee1a13b0fe7e774d5b2fedf3ada0
Ali11Mustafa <ali.00622274@gmail.com>
2025-06-28 22:01:36 +0300

    changes

--- src/Components/Data/quota.ts
+++ src/Components/Data/quota.ts
@@ -3,7 +3,7 @@
 import P1 from "../../../public/ClinetPics/client-profile.svg";
 import p2 from '../../../public/ClinetPics/Jonatan.png';
 import p3 from '../../../public/ClinetPics/Marry.png';
-import { Client } from "../Types/Quota" 
+import { Client } from "../types/Quota" 
 
 export const clients: Client[] = [
   {
--- src/Components/Pages/ClientQuota/ClientQuota/ClientQuta.tsx
+++ src/Components/Pages/ClientQuota/ClientQuota/ClientQuta.tsx
@@ -5,7 +5,7 @@ import Image from 'next/image';
 import { AiOutlineRight, AiOutlineLeft } from 'react-icons/ai';
 
 import { clients } from '../../../Data/quota';
-import { Client } from "../../../Types/Quota";
+import { Client } from "../../../types/Quota";
 interface QuoteCardProps {
   client: Client;
 }
